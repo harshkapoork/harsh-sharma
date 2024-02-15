11ff160b13aaa01e91d827dfb47b8b5ef80994d7
harsh kapoor <harshkapoor458@gmail.com>
2024-02-15 23:30:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+// const port = process.env.PORT ||1001
+const port =  1001;
+const path=require("path")
+app.listen(port, () => {
+    console.log("server is created at",port)
+})
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"assets")))
+
+app.get("/", (req, res) => {
+        console.log("/")
+        res.render("home.ejs");
+})
+app.get("/home", (req, res) => {
+    console.log("/")
+    res.render("home.ejs");
+})
+
+app.get("/about", (req, res) => {
+    console.log("/about")
+    res.render("about.ejs");
+})
+app.get("/contact", (req, res) => {
+    console.log("/contact")
+    res.render("contact.ejs");
+})
+app.get("/plans", (req, res) => {
+    console.log("/plans")
+    res.render("plans.ejs");
+})
+app.get("*", (req, res) => {
+    console.log("/")
+    res.send("404 Page not found");
+})
